9a5ea56a692a16a81bb8f9dfa07103bd2ba29a5c
Jack 299@@ <sukanyaonchaiya@gmail.com>
2025-06-20 21:55:42 +0700

    Refa

---
+++ script/Refa.js
@@ -0,0 +1,23 @@
+(function () {
+
+    const inactivityDuration = 1 * 60 * 1000;
+    let inactivityTimer;
+
+    function redirectToLogin() {
+
+        window.location.href = './index.html';
+    }
+
+    function resetInactivityTimer() {
+        clearTimeout(inactivityTimer);
+        inactivityTimer = setTimeout(redirectToLogin, inactivityDuration);
+    }
+
+
+    window.addEventListener('mousemove', resetInactivityTimer, false);
+    window.addEventListener('mousedown', resetInactivityTimer, false);
+    window.addEventListener('keypress', resetInactivityTimer, false);
+    window.addEventListener('touchmove', resetInactivityTimer, false);
+    window.addEventListener('scroll', resetInactivityTimer, true);
+    resetInactivityTimer();
+})();
